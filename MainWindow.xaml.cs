b14fa5d35411dcbf155eb5f84fbd6bb6516e941c
Todor Totev <teobugslayer@gmail.com>
2020-04-17 18:00:22 +0300

    Add dependency to System.Reactive 4.4.1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,11 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using ReactiveUI;
+using System;
+using Path = System.IO.Path;
 
 namespace AvaloniaCoreRTDemo
 {
@@ -9,11 +14,35 @@ namespace AvaloniaCoreRTDemo
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new ViewModel();
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+    }
+
+    public class ViewModel: ReactiveObject
+    {
+        public ViewModel()
+        {
+            DotNetImage = new Bitmap(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dotnet.png"));
+            AvaloniaImage = new Bitmap(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "avalonia.png"));
+        }
+
+        private IBitmap dotNetImage;
+        public IBitmap DotNetImage
+        {
+            get { return dotNetImage; }
+            set { this.RaiseAndSetIfChanged(ref this.dotNetImage, value); }
+        }
+
+        private IBitmap avaloniaImage;
+        public IBitmap AvaloniaImage
+        {
+            get { return avaloniaImage; }
+            set { this.RaiseAndSetIfChanged(ref this.avaloniaImage, value); }
         }
     }
 }
---
+++ test.cmd
@@ -0,0 +1,2 @@
+dotnet publish -o Publish  -r win-x64 -c release 
+rem dotnet publish -o Publish  -r win-x64 -c release /p:RestoreLockedMode=true
