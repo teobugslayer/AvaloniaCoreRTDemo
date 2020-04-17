c199722e817c405586e129a58432d7d8bce8a389
Todor Totev <teobugslayer@gmail.com>
2020-04-17 22:37:58 +0300

    Added main menu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,10 +2,6 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Markup.Xaml;
-using Avalonia.Media.Imaging;
-using ReactiveUI;
-using System;
-using Path = System.IO.Path;
 
 namespace AvaloniaCoreRTDemo
 {
@@ -14,35 +10,12 @@ namespace AvaloniaCoreRTDemo
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new ViewModel();
+            this.DataContext = new MainWindowViewModel();
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-    }
-
-    public class ViewModel: ReactiveObject
-    {
-        public ViewModel()
-        {
-            DotNetImage = new Bitmap(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dotnet.png"));
-            AvaloniaImage = new Bitmap(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "avalonia.png"));
-        }
-
-        private IBitmap dotNetImage;
-        public IBitmap DotNetImage
-        {
-            get { return dotNetImage; }
-            set { this.RaiseAndSetIfChanged(ref this.dotNetImage, value); }
-        }
-
-        private IBitmap avaloniaImage;
-        public IBitmap AvaloniaImage
-        {
-            get { return avaloniaImage; }
-            set { this.RaiseAndSetIfChanged(ref this.avaloniaImage, value); }
         }
     }
 }
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Avalonia;
+using Avalonia.Media.Imaging;
+using ReactiveUI;
+using System;
+using System.Reactive;
+using Path = System.IO.Path;
+
+namespace AvaloniaCoreRTDemo
+{
+    public class MainWindowViewModel: ReactiveObject
+    {
+        public MainWindowViewModel()
+        {
+            DotNetImage = new Bitmap(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dotnet.png"));
+            AvaloniaImage = new Bitmap(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "avalonia.png"));
+
+            FileExitCommand = ReactiveCommand.Create(RunFileExit);
+            HelpAboutCommand = ReactiveCommand.Create(RunHelpAbout);
+        }
+        
+        public IBitmap DotNetImage
+        {
+            get { return dotNetImage; }
+            set { this.RaiseAndSetIfChanged(ref this.dotNetImage, value); }
+        }
+
+        public IBitmap AvaloniaImage
+        {
+            get { return avaloniaImage; }
+            set { this.RaiseAndSetIfChanged(ref this.avaloniaImage, value); }
+        }
+
+        public ReactiveCommand<Unit, Unit> FileExitCommand { get; }
+        public ReactiveCommand<Unit, Unit> HelpAboutCommand { get; }
+
+        void RunFileExit()
+        {
+            Environment.Exit(0);
+        }
+
+        void RunHelpAbout()
+        {
+            // Code for executing the command here.
+        }
+
+        private IBitmap dotNetImage;
+        private IBitmap avaloniaImage;
+    }
+}
