927109ff250aa8ff7f43b4cb2dd013869a21db92
Todor Totev <teobugslayer@gmail.com>
2020-04-17 22:39:16 +0300

    Made it work, but still some debug code remains.

--- test.cmd
+++ test.cmd
@@ -1,2 +1,1 @@
-rem dotnet publish -o Publish  -r win-x64 -c release 
-dotnet publish -o Publish  -r win-x64 -c release /p:RestoreLockedMode=true
+dotnet publish -r win-x64 -c release /p:RestoreLockedMode=true
