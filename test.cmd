1ded5ab913ed3f43e415e1cea28e7d6f69902b94
Todor Totev <teobugslayer@gmail.com>
2020-04-17 18:06:33 +0300

    updated dependencies

--- test.cmd
+++ test.cmd
@@ -1,2 +1,2 @@
-dotnet publish -o Publish  -r win-x64 -c release 
-rem dotnet publish -o Publish  -r win-x64 -c release /p:RestoreLockedMode=true
+rem dotnet publish -o Publish  -r win-x64 -c release 
+dotnet publish -o Publish  -r win-x64 -c release /p:RestoreLockedMode=true
