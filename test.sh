18c8883f182d1e41c9ee0b12a5606e6f1952cc0e
Maksym Katsydan <maxkatz6@outlook.com>
2022-11-12 22:23:41 -0500

    Update test scripts

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f src/packages.lock.json
 dotnet publish -r linux-x64 -c release /p:RestoreLockedMode=true /p:TrimLink=true --self-contained 
-cd src/bin/x64/Release/net6.0/linux-x64/publish 
+cd src/bin/x64/Release/net7.0/linux-x64/publish 
 cp AvaloniaCoreRTDemo AvaloniaCoreRTDemo.bin 
 strip AvaloniaCoreRTDemo.bin 
