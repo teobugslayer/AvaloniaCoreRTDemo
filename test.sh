ffcfefa0659e5f0c0c3589c07be144f34bcccea9
Joseph Moreno <44370115+josephmoresena@users.noreply.github.com>
2023-02-10 13:37:09 -0500

    Strip only for release

--- test.sh
+++ test.sh
@@ -3,4 +3,3 @@ rm -f src/packages.lock.json
 dotnet publish -r linux-x64 -c Release /p:RestoreLockedMode=true
 cd src/bin/Release/net7.0/linux-x64/publish 
 cp AvaloniaCoreRTDemo AvaloniaCoreRTDemo.bin 
-strip AvaloniaCoreRTDemo.bin 
